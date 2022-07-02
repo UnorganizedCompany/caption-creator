87a24075e23364c953245918e7e5c16d95a22d12
JettiJAM <jettijam@gmail.com>
2022-07-02 21:03:16 +0900

    transparent background

--- caption.py
+++ caption.py
@@ -8,8 +8,9 @@ import sys
 class CaptionCreator():
     space_between_name_text = 10
     space_between_lines = 10
+    space_bootom = 50
     def __init__(self, texts):
-        namefnt = ImageFont.truetype("./DOSMyungjo.ttf", encoding="UTF-7", size=50)
+        namefnt = ImageFont.truetype("./DOSMyungjo.ttf", encoding="UTF-7", size=65)
         name = ""
         if ":" in texts[0]:
             splitted = texts[0].split(":")
@@ -17,7 +18,7 @@ class CaptionCreator():
             texts[0] = ":".join(splitted[1:])
         nameW, nameH = namefnt.getsize(name)
 
-        fnt = ImageFont.truetype("./DOSMyungjo.ttf", encoding="UTF-8", size=50)
+        fnt = ImageFont.truetype("./DOSMyungjo.ttf", encoding="UTF-8", size=65)
         textH = 0
         textW = -1
         for s in texts:
@@ -30,26 +31,33 @@ class CaptionCreator():
 
         textH += self.space_between_lines*(len(texts)-1)
 
-        imgW = nameW + self.space_between_name_text + textW
-        imgH = textH if textH > nameH else nameH
+        capW = nameW + self.space_between_name_text + textW
+        capH = textH if textH > nameH else nameH
 
-        self.im = Image.new("RGBA", (imgW, imgH), (0, 0, 0, 128))
+        self.im = Image.new("RGBA", (1920, 1080), (0, 0, 0, 0))
+
+        capX = (1920-capW)//2
+        capY = 1080-(self.space_bootom + capH)
 
         # color 242, 242, 242 for name
         pxs = self.im.load()
         for y in range(nameH):
             for x in range(nameW):
-                pxs[x, y] = (242, 242, 242)
+                pxs[capX+x, capY+y] = (242, 242, 242)
         
         pxs = self.im.load()
-        for y in range(imgH-nameH):
+        for y in range(capH-nameH):
             for x in range(nameW):
-                pxs[x, nameH+y] = (0, 0, 0, 0)
+                pxs[capX+x, capY+nameH+y] = (0, 0, 0, 0)
+
+        for y in range(textH):
+            for x in range(textW):
+                pxs[capX+nameW+x, capY+y] = (0, 0, 0, 128)
 
         draw = ImageDraw.Draw(self.im)
-        draw.multiline_text((0,0), name, font=fnt, fill=(0, 0, 0))
+        draw.multiline_text((capX,capY), name, font=fnt, fill=(0, 0, 0))
        
-        draw.multiline_text((nameW+self.space_between_name_text,0), "\n".join(texts), font=fnt, fill=(255, 255, 255), 
+        draw.multiline_text((capX+nameW+self.space_between_name_text,capY), "\n".join(texts), font=fnt, fill=(255, 255, 255), 
                                 align="left", spacing=self.space_between_lines)
 
     def show(self):
